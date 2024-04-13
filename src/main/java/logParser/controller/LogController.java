b11dd0b25f933c8286f3982d7f8f6bd5e171b74c
Irene Spanou <2804507+iresil@users.noreply.github.com>
2024-04-13 18:00:19 +0300

    Avoided exceptions if file parsing isn't successful

--- src/main/java/logParser/controller/LogController.java
+++ src/main/java/logParser/controller/LogController.java
@@ -52,13 +52,15 @@ public class LogController {
     @GetMapping("/top10Resources")
     public JSONArray getMostRequestedResources() {
         JSONArray top10Resources = new JSONArray();
-        JSONObject resource;
-        List<Map.Entry> entries = data.getResourcesSortedByFrequency().stream().limit(10).collect(Collectors.toList());
-        for (Map.Entry entry : entries) {
-            resource = new JSONObject();
-            resource.appendField("resource", entry.getKey());
-            resource.appendField("requests", entry.getValue());
-            top10Resources.add(resource);
+        if (data != null) {
+            JSONObject resource;
+            List<Map.Entry> entries = data.getResourcesSortedByFrequency().stream().limit(10).collect(Collectors.toList());
+            for (Map.Entry entry : entries) {
+                resource = new JSONObject();
+                resource.appendField("resource", entry.getKey());
+                resource.appendField("requests", entry.getValue());
+                top10Resources.add(resource);
+            }
         }
         return top10Resources;
     }
@@ -66,32 +68,38 @@ public class LogController {
     @GetMapping("/successPercentage")
     public JSONObject getSuccessfulRequestPercentage() {
         JSONObject successfulPercentage = new JSONObject();
-        NumberFormat nf = NumberFormat.getInstance(Locale.US);
-        nf.setMaximumFractionDigits(3);
-        Double percentage = (data.getSuccessfulRequests() * 1.0 / data.getAllRequests()) * 100;
-        successfulPercentage.appendField("successful request percentage", nf.format(percentage));
+        if (data != null) {
+            NumberFormat nf = NumberFormat.getInstance(Locale.US);
+            nf.setMaximumFractionDigits(3);
+            Double percentage = (data.getSuccessfulRequests() * 1.0 / data.getAllRequests()) * 100;
+            successfulPercentage.appendField("successful request percentage", nf.format(percentage));
+        }
         return successfulPercentage;
     }
 
     @GetMapping("/failPercentage")
     public JSONObject getFailedRequestPercentage() {
         JSONObject failedPercentage = new JSONObject();
-        NumberFormat nf = NumberFormat.getInstance(Locale.US);
-        nf.setMaximumFractionDigits(3);
-        int failedRequests = data.getAllRequests() - data.getSuccessfulRequests();
-        Double percentage = ((failedRequests) * 1.0 / data.getAllRequests()) * 100;
-        failedPercentage.appendField("failed request percentage", nf.format(percentage));
+        if (data != null) {
+            NumberFormat nf = NumberFormat.getInstance(Locale.US);
+            nf.setMaximumFractionDigits(3);
+            int failedRequests = data.getAllRequests() - data.getSuccessfulRequests();
+            Double percentage = ((failedRequests) * 1.0 / data.getAllRequests()) * 100;
+            failedPercentage.appendField("failed request percentage", nf.format(percentage));
+        }
         return failedPercentage;
     }
 
     @GetMapping("/top10FailingResources")
     public JSONArray getFrequentlyFailingResources() {
         JSONArray top10Failed = new JSONArray();
-        JSONObject failedRequest;
-        for (String request : data.getTop10FailedResources()) {
-            failedRequest = new JSONObject();
-            failedRequest.appendField("resource", request);
-            top10Failed.add(failedRequest);
+        if (data != null) {
+            JSONObject failedRequest;
+            for (String request : data.getTop10FailedResources()) {
+                failedRequest = new JSONObject();
+                failedRequest.appendField("resource", request);
+                top10Failed.add(failedRequest);
+            }
         }
         return top10Failed;
     }
@@ -99,12 +107,14 @@ public class LogController {
     @GetMapping("/top10Hosts")
     public JSONArray getFrequentlyAppearingHosts() {
         JSONArray top10Hosts = new JSONArray();
-        JSONObject host;
-        for (Map.Entry request : data.getTop10HostResources()) {
-            host = new JSONObject();
-            host.appendField("host", request.getKey());
-            host.appendField("requests", ((List<String>)request.getValue()).size());
-            top10Hosts.add(host);
+        if (data != null) {
+            JSONObject host;
+            for (Map.Entry request : data.getTop10HostResources()) {
+                host = new JSONObject();
+                host.appendField("host", request.getKey());
+                host.appendField("requests", ((List<String>) request.getValue()).size());
+                top10Hosts.add(host);
+            }
         }
         return top10Hosts;
     }
@@ -112,21 +122,23 @@ public class LogController {
     @GetMapping("/top5RequestsForTop10Hosts")
     public JSONArray getFrequentRequestsForFrequentlyAppearingHosts() {
         JSONArray top10HostsTop5Requests = new JSONArray();
-        JSONObject hostObject;
-        JSONArray hostRequests;
-        JSONObject hostRequestObj;
-        for (Map.Entry hostRequest : new ArrayList<Map.Entry>(data.getTop10HostRequests().entrySet())) {
-            hostObject = new JSONObject();
-            hostObject.appendField("host", hostRequest.getKey());
-            hostRequests = new JSONArray();
-            for (Map.Entry hostRequestCount : (List<Map.Entry>)hostRequest.getValue()) {
-                hostRequestObj = new JSONObject();
-                hostRequestObj.appendField("resource", hostRequestCount.getKey());
-                hostRequestObj.appendField("count", hostRequestCount.getValue());
-                hostRequests.add(hostRequestObj);
+        if (data != null) {
+            JSONObject hostObject;
+            JSONArray hostRequests;
+            JSONObject hostRequestObj;
+            for (Map.Entry hostRequest : new ArrayList<Map.Entry>(data.getTop10HostRequests().entrySet())) {
+                hostObject = new JSONObject();
+                hostObject.appendField("host", hostRequest.getKey());
+                hostRequests = new JSONArray();
+                for (Map.Entry hostRequestCount : (List<Map.Entry>)hostRequest.getValue()) {
+                    hostRequestObj = new JSONObject();
+                    hostRequestObj.appendField("resource", hostRequestCount.getKey());
+                    hostRequestObj.appendField("count", hostRequestCount.getValue());
+                    hostRequests.add(hostRequestObj);
+                }
+                hostObject.appendField("requests", hostRequests);
+                top10HostsTop5Requests.add(hostObject);
             }
-            hostObject.appendField("requests", hostRequests);
-            top10HostsTop5Requests.add(hostObject);
         }
         return top10HostsTop5Requests;
     }
