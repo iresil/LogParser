6c1137bdc6343646858fbfede50fe1aef6459e63
Irene Spanou <2804507+iresil@users.noreply.github.com>
2024-04-20 23:07:20 +0300

    Updated javadocs

--- src/main/java/logParser/util/StatisticsCalculator.java
+++ src/main/java/logParser/util/StatisticsCalculator.java
@@ -48,7 +48,8 @@ public class StatisticsCalculator {
     /**
      * Sorts the resources contained in resourceCallCount by number of calls performed
      * @param resourceCallCount A HashMap of Strings and Integers containing the number of times each resource was called
-     * @return A List of Entries containing Strings and Integers, sorted by the Integer values, descending
+     * @return A LinkedHashMap containing the resources as its keys and the number of times each resource was requested as
+     * its values, sorted by the number of calls in descending order
      */
     public static LinkedHashMap<String, Integer> sortResourcesByFrequency(HashMap<String, Integer> resourceCallCount) {
         return resourceCallCount.entrySet().stream()
@@ -59,7 +60,8 @@ public class StatisticsCalculator {
     /**
      * Sorts the resources contained in resourceCallCount by number of calls performed
      * @param resourceFailCount A HashMap of Strings and Integers containing the number of times each resource was called and failed
-     * @return A List of Entries containing Strings and Integers, sorted by the Integer values, descending
+     * @return A LinkedHashMap containing the resources as its keys and the number of times each resource call failed
+     * as its values, sorted by the number of failed calls in descending order
      */
     public static LinkedHashMap<String, Integer> sortFailedResourcesByFrequency(HashMap<String, Integer> resourceFailCount) {
         return resourceFailCount.entrySet().stream()
@@ -70,7 +72,8 @@ public class StatisticsCalculator {
     /**
      * Sorts the hosts contained in requestsPerHost by number of requests performed
      * @param requestsPerHost A HashMap of Strings and Lists of Strings containing the requests that were made by each host
-     * @return A List of Entries containing Strings and Lists of Strings, sorted by the inner Lists' size, descending
+     * @return A LinkedHashMap containing the hosts as its keys and a list of resources per host as its values,
+     * sorted by the resource Lists' size in descending order
      */
     public static LinkedHashMap<String, List<String>> sortHostsByRequestFrequency(HashMap<String, List<String>> requestsPerHost) {
         return requestsPerHost.entrySet().stream()
@@ -80,8 +83,9 @@ public class StatisticsCalculator {
 
     /**
      * Retrieves the requested resources for the top 10 most frequently appearing hosts
-     * @param hostsSortedByCallFrequency A List of Entries containing Strings and Lists of Strings, sorted by the inner Lists' size, descending
-     * @return The first 10 entries found in the input
+     * @param hostsSortedByCallFrequency A LinkedHashMap containing the hosts as its keys and a list of resources per host
+     *                                   as its values, sorted by the resource Lists' size in descending order
+     * @return The first 10 entries found in the input LinkedHashMap
      */
     public static LinkedHashMap<String, List<String>> getAllRequestsForTopHosts(LinkedHashMap<String, List<String>> hostsSortedByCallFrequency) {
         return hostsSortedByCallFrequency.entrySet().stream().limit(10)
@@ -90,8 +94,10 @@ public class StatisticsCalculator {
 
     /**
      * Retrieves the top 10 most often failing resources
-     * @param failedResourcesSortedByFrequency A List of Entries containing Strings and Integers, sorted by the Integer values, descending
-     * @return The first 10 values that contain failed requests
+     * @param failedResourcesSortedByFrequency A LinkedHashMap containing the resources as its keys and the number of times
+     *                                         each resource call failed as its values, sorted by the number of failed
+     *                                         calls in descending order
+     * @return The first 10 keys found in the LinkedHashMap
      */
     public static String[] getFrequentlyFailingResources(LinkedHashMap<String, Integer> failedResourcesSortedByFrequency) {
         String[] top10FailedResources = new String[10];
@@ -108,8 +114,11 @@ public class StatisticsCalculator {
 
     /**
      * Calculates the number of calls for the top 5 resources that were called by the top 10 hosts
-     * @param top10HostResources A List of Entries containing Strings and Lists of Strings, corresponding to the top 10 hosts in frequency
-     * @return A HashMap of Strings and Lists of Entries containing Strings and Integers, corresponding to the top 5 requested resources for each of the top 10 hosts
+     * @param top10HostResources A LinkedHashMap containing the top 10 hosts as its keys and a list of resources per host
+     *                           as its values, sorted by the resource Lists' size in descending order
+     * @return A LinkedHashMap containing the hosts as its keys, with a LinkedHashMap per host, which in turn contains
+     * resource names as its keys and the number of calls per resource as its values - the entire structure corresponds
+     * to the top 5 requested resources for each one of the top 10 hosts
      */
     public static LinkedHashMap<String, LinkedHashMap<String, Long>> getFrequentRequestsPerHost(LinkedHashMap<String, List<String>> top10HostResources) {
         LinkedHashMap<String, LinkedHashMap<String, Long>> top10HostRequests = new LinkedHashMap<>();
